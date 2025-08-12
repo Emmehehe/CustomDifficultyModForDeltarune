187e2be86d04e885c25e227eb9090dd7cd05ed48
EmmaR <emmarawlingswastaken@gmail.com>
2025-08-12 01:10:45 +0100

    Add new folder name to installer

--- customdifficulty_installer_windows.bat
+++ customdifficulty_installer_windows.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Installing mod menu and custom difficulty for all chapters...
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter1_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter2_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter3_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter4_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "diffops_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter1_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "diffops_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter2_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "diffops_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter3_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "diffops_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter4_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter1_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter2_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter3_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter4_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter1_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter2_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter3_windows\data.win"
+"customdifficulty_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "customdifficulty_installfiles\Scripts\customdifficulty_ch1to4.csx" --verbose false --output "chapter4_windows\data.win"
 echo Finished installation.
 pause
